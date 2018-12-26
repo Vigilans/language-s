1da6266b53fc53f7d7fa65d64715c0395a4a9d74
Vigilans <vigilans@foxmail.com>
2018-12-26 17:37:47 +0800

    Some more PRC functions

--- src/Primitive.hs
+++ src/Primitive.hs
@@ -2,64 +2,66 @@ module Primitive where
 
 import Program
 import Function
-import Data.Sequence (Seq(..), (<|))
+import Data.Map ((!))
 import Data.Foldable (toList)
-import qualified Data.Sequence as S
 import qualified Control.Monad.State as M
+import Debug.Trace
 
--- Operators
+-- Primitive Operators
 
 con :: Function -> [Function] -> Function
 con f gs | (length gs == k) && all (\g -> argv g == n) (tail gs) =
-    function n $ \(y, xs) -> do
-        zs <- freeVars k
+    function n $ \(y, xs) -> context k 0 $ \(zs, []) -> do
+        (_, State vars _, _) <- M.get
         mapM_ (\(g, z) -> call g (z, xs)) $ zip gs zs
         call f (y, zs)
-        ret y
+        curAddr
     where k = argv f
           n = argv $ head gs
 
 rec :: Function -> Function -> Function
-rec f g | argv g == n + 2 =
-    function (n + 1) $ \(y, xx) -> do
-        let (xs :|> t) = S.fromList xx
+rec f g | argv g == n + 2 = function (n + 1) $ \(y, t : xs) ->
+    context 1 1 $ \([z], [a]) -> do
         (_, _, exit) <- M.get
-        [z] <- freeVars 1
-        [a] <- freeLabels 1
-        call f (y, toList xs)
+        call f (y, xs)
         _label_ a
         gz t exit
-        call g (y, toList $ z <| y <| xs)
+        call g (y, z : y : xs)
+        addr <- curAddr
         inc z
         dec t
         goto a
-        ret y
     where n = argv f
 
-rev :: Function -> Function
-rev f = function (argv f) $ \(y, xs) -> do
-    case xs of
-        []     -> call f (y, [])
-        (x:xs) -> call f (y, xs ++ [x])
-    ret y
-
-(%) :: Function -> Int -> Function
-(Function _ f) % n = Function n f -- rewrite argv
+(%) :: Function -> Int -> Function -- rewrite argv
+(Function _ f) % n = Function n f
 
 -- Initial Functions
 
-n :: Function
-n = nullary $ \(_, _) -> ret false
+z :: Function
+z = nullary $ \(y, _) -> mov y false
 
 s :: Function
-s = unary $ \(y, [x]) -> do
-    inc x
-    ret x
+s = unary $ \(y, [x]) -> inc x >> mov y x
 
 u :: Int -> Int -> Function
-u n i = function n $ \(y, xs) -> ret $ xs !! i
+u n i = function n $ \(y, xs) -> mov y $ xs !! i
 
-k :: Value -> Function
-k n = nullary $ \(y, []) -> do
+-- Basic Functions and Operators
+
+id' :: Function
+id' = u 1 0
+
+iota :: Function -> Function -- Nth iteration of f
+iota f = rec id' (con f [u (2 + argv f) 1])
+
+(^) :: Function -> Value -> Function
+f ^ n = unary $ \(y, [x]) -> do
     set y n
-    ret y
+    call (iota s) (y, [y, x])
+
+k :: Value -> Function -- Constant k
+k n = con (s Primitive.^ n) [z]
+
+rev :: Function -> Function -- Flip argument list
+rev f | argv f == 2 = con f [u 2 1, u 2 0]
