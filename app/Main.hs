51ec3750494c42538c418e28bfd96ff3f718f4e4
Vigilans <vigilans@foxmail.com>
2018-12-26 09:44:12 +0800

    Add some primitive recursive functions

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,12 @@ import Primitive
 
 -- Primitive recursive functions
 
-add' = rec (u 1 0) (con s [u 3 1])
+add = rec (u 1 0) (con s [u 3 1])
+mul = rec n (con add [u 3 1, u 3 2])
+fac = rec (k 1) (con mul [con s [u 2 0], u 2 1])
+pow = rec (con s [n]) (con mul [u 3 1, u 3 2])
+pre = rec (k 0) (u 2 0)
+sub = rec (u 1 0) (con pre [u 3 1])
 
 main :: IO ()
 main = print "Hello"
